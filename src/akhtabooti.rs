a01e3ac8a60cb8b0a724ed933de098accc2fe88c
AcePhire <acephire27@gmail.com>
2026-07-23 17:02:26 +0300

    Add single file search

---
+++ src/akhtabooti.rs
@@ -0,0 +1,62 @@
+use crate::text_utils;
+use crate::file_utils;
+
+use std::collections::HashSet;
+use walkdir::WalkDir;
+use std::io;
+use std::path::Path;
+use serde::{Serialize, Deserialize};
+
+#[derive(Default, Serialize, Deserialize, Debug)]
+pub struct FilePIIs {
+    filename: String,
+    email_accounts: HashSet<String>,
+    phone_numbers: HashSet<String>,
+    other_piis: Vec<String>
+}
+
+fn search_text_for_pii(filename: &str, text: &str) -> Result<FilePIIs, io::Error> {
+    let regex = text_utils::get_regexes("definitions.json");
+    let rules = regex.unwrap();
+
+    let pii = FilePIIs {
+        filename: filename.to_string(),
+        email_accounts: text_utils::detect_emails(rules.clone(), text).unwrap(),
+        phone_numbers: text_utils::detect_phone_numbers(rules.clone(), text).unwrap(),
+        other_piis: text_utils::detect_keywords(rules.clone(), text).unwrap()
+    };
+
+    Ok(pii)
+}
+
+pub fn search_directory(path: &str) -> Result<Vec<FilePIIs>, io::Error> {
+    let mut piis: Vec<FilePIIs> = Vec::new(); 
+
+    for entry in WalkDir::new(path) {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        if path.is_file() {
+            let filepath = entry.path().to_str().unwrap();
+            let text = file_utils::extract_text(&filepath);
+
+            let file_pii = search_text_for_pii(&filepath, &text).unwrap();
+            piis.push(file_pii);
+        }
+    }
+
+    Ok(piis)
+}
+
+pub fn search_file(path: &str) -> Result<FilePIIs, io::Error> {
+    let filepath = Path::new(path);
+
+    if filepath.is_file() {
+        let text = file_utils::extract_text(&path);
+
+        let file_pii = search_text_for_pii(&path, &text).unwrap();
+        return Ok(file_pii);
+    }
+
+    Ok(FilePIIs::default())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,17 @@
-mod text_utils;
 mod file_utils;
+mod text_utils;
+mod akhtabooti;
 
-use std::env;
-use std::collections::HashSet;
-use walkdir::WalkDir;
+use serde_json;
 use std::io;
-use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct FilePIIs {
-    email_accounts: HashSet<String>,
-    phone_numbers: HashSet<String>,
-    other_piis: Vec<String>
-}
-
-fn main() {
-    fn search_for_pii(text: &str) -> Result<(), Box<dyn std::error::Error>> {
-        let regex = text_utils::get_regexes("definitions.json");
-        let rules = regex.unwrap();
-
-        let pii = FilePIIs {
-            email_accounts: text_utils::detect_emails(rules.clone(), text).unwrap(),
-            phone_numbers: text_utils::detect_phone_numbers(rules.clone(), text).unwrap(),
-            other_piis: text_utils::detect_keywords(rules.clone(), text).unwrap()
-        };
-
-        let json = serde_json::to_string(&pii)?;
-
+fn main() -> Result<(), io::Error> {
+    let piis = akhtabooti::search_directory("pii/");
+    for i in piis {
+        let json = serde_json::to_string(&i)?;
+        
         println!("{}", json);
-        Ok(())
     }
 
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
-
-    for entry in WalkDir::new(path) {
-        let entry = entry.unwrap();
-        let path = entry.path();
-
-        if path.is_file() {
-            let filename = entry.path().to_str().unwrap();
-            let text = file_utils::extract_text(&filename);
-
-            search_for_pii(&text);
-        }
-    }
-
-    // for entry in WalkDir::new(path) {
-    //     let entry = entry.unwrap();
-    //     let path = entry.path();
-    //
-    //     if path.is_file() {
-    //         let filename = path.file_name().unwrap().to_string_lossy().into_owned();
-    //         let text = file_utils::extract_text(&filename);
-    //
-    //         println!("{:?}", text);
-    //
-    //         let _piis = search_for_pii(&text);
-    //     }
-    // }
+    Ok(())
 }
